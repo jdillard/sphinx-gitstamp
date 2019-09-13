a3a30be0819a33a3b692a60e373b3fb23472433f
Jared Dillard <jared.dillard@gmail.com>
2019-09-12 22:32:46 -0700

    prep for v0.2.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,13 @@
 Changelog
 =========
 
+0.2.0
+-----
+
+*Release date: 2019-09-12*
+
+* Fix PyPI description
+
 0.1.0
 -----
 
--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     install_requires=['six', 'sphinx >= 1.2'],
     url="https://github.com/jdillard/sphinx-gitstamp",
     license='MIT',
-    download_url="https://github.com/jdillard/sphinx-gitstamp/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/jdillard/sphinx-gitstamp/archive/v0.2.0.tar.gz",
     packages=['sphinx_gitstamp'],
  )
