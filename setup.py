0a10fa8d482b4556877fa06251a8869980c18ab8
Jared Dillard <jared.dillard@gmail.com>
2022-12-14 21:47:24 -0800

    Update the build process

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
-import os
+import setuptools
 
-from setuptools import setup
-
-import sphinx_gitstamp
-
-long_description = open(
-    "README.rst" if os.path.exists("README.rst") else "README.md"
-).read()
-
-setup(
-    name="sphinx-gitstamp",
-    description="git timestamp generator for Sphinx",
-    long_description=long_description,
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Documentation :: Sphinx",
-        "Programming Language :: Python :: 3",
-        "Framework :: Sphinx :: Extension",
-    ],
-    version=sphinx_gitstamp.__version__,
-    author="Jared Dillard",
-    author_email="jared.dillard@gmail.com",
-    install_requires=["six", "sphinx >= 1.2", "gitpython"],
-    url="https://github.com/jdillard/sphinx-gitstamp",
-    license="MIT",
-    packages=["sphinx_gitstamp"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
