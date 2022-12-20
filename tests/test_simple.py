5e8e97cdc046f5c9381bbaebda5835b96acd0106
Jared Dillard <jared.dillard@gmail.com>
2022-12-19 22:34:34 -0800

    Add testing infrastructure

---
+++ tests/test_simple.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.sphinx(
+    "html",
+    freshenv=True,
+    confoverrides={"gitstamp_fmt": "%b %d, %Y"},
+)
+def test_simple(app, status, warning):
+    app.build()
+    assert "bar.html" in app.outdir.listdir()
